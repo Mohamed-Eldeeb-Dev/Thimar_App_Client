87ef165f029e1ce4a8a57ff22502afebf7219ee0
Mohamed-Eldeeb-Dev <eldeebmohamed237@gmail.com>
2025-09-18 20:18:10 +0300

    add SplashScreen with animations and navigation handling

---
+++ lib/features/intro/splash/splash_screen.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:thimar_app/core/routes/app_routes_fun.dart';
+import 'package:thimar_app/core/routes/routes.dart';
+import 'package:thimar_app/gen/assets.gen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController logoController;
+  late Animation<Offset> logoAnimation;
+
+  late AnimationController leavesController;
+  late Animation<Offset> leavesAnimation;
+
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    logoController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+    logoAnimation = Tween<Offset>(
+      begin: Offset(0, 2),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: logoController, curve: Curves.easeOut));
+    leavesController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+    leavesAnimation = Tween<Offset>(
+      begin: Offset(0, -10),
+      end: Offset(0, -0.55),
+    ).animate(CurvedAnimation(parent: leavesController, curve: Curves.easeOut));
+
+    logoController.forward().then((_) {
+      leavesController.forward();
+      
+      _timer = Timer(Duration(seconds: 2), () {
+        if (mounted) {
+          AppRoutesFunc.replacement(NamedRoutes.login);
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    logoController.dispose();
+    leavesController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: -70,
+            left: 50,
+            child: Image.asset(Assets.images.vegetablesPaper.path),
+          ),
+          Positioned.fill(
+            child: Image.asset(Assets.images.wall.path, fit: BoxFit.cover),
+          ),
+          Center(
+            child: SlideTransition(
+              position: logoAnimation,
+              child: Stack(
+                children: [
+                  Image.asset(Assets.images.logo.path, width: 180),
+                  SlideTransition(
+                    position: leavesAnimation,
+                    child: Image.asset(Assets.images.leaves.path, width: 180),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:thimar_app/core/routes/app_routes.dart';
+import 'package:thimar_app/core/routes/app_routes_fun.dart';
+import 'package:thimar_app/core/utils/app_theme.dart';
+
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+    statusBarBrightness: Brightness.light,
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
   runApp(const MyApp());
 }
 
@@ -9,16 +22,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'thimar app',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello, Thimar'),
-        ),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Thimar',
+          themeMode: ThemeMode.light,
+          theme: AppThemes.lightTheme,
+          darkTheme: AppThemes.lightTheme,
+          initialRoute: AppRoutes.init.initial,
+          routes: AppRoutes.init.appRoutes,
+          navigatorKey: AppRoutesFunc.navigator,
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
